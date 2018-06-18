d2de6f03463e7f302a045d7e64002a6d5ab4412e
andberger <and.bergsson@gmail.com>
2018-06-18 15:13:21 +0200

    Add mean and confidence interval calculations

--- post_office.py
+++ post_office.py
@@ -11,6 +11,7 @@ def single_queue_multiple_servers_simulation(n_customers, n_servers):
     mean_time_between_customers = 1
     mean_service_time = 8
     waiting_times = []
+    blocked = [0 for _ in range(n_customers)]
     arrival_dist = stats.expon.rvs(size=n_customers, scale=mean_time_between_customers)
     service_dist = stats.expon.rvs(size=n_customers, scale=mean_service_time)
     
@@ -27,13 +28,14 @@ def single_queue_multiple_servers_simulation(n_customers, n_servers):
         if server > arrival_time:
             waiting_time = server - arrival_time
             server = server + service_dist[i]
+            blocked[i] = 1
         else:
             server = arrival_time + service_dist[i]
         
         servers[server_index] = server
         waiting_times.append(waiting_time)
         
-    return waiting_times
+    return (waiting_times, blocked)
     
 def calculate_confidence_intervals(mean, standard_deviation, n_simulations):
     z_s = stats.t.ppf(0.95, n_simulations)
@@ -44,15 +46,37 @@ def calculate_confidence_intervals(mean, standard_deviation, n_simulations):
 
 def main():    
     # Single queue multiple servers    
-    waiting_times = single_queue_multiple_servers_simulation(10000, 10)
+    waiting_times, _ = single_queue_multiple_servers_simulation(10000, 10)
     waiting_times_strip_zeros = [i for i in waiting_times if i > 0]
     
-    plt.hist(waiting_times_stip_zeros,  alpha=0.5)
+    plt.hist(waiting_times_strip_zeros, alpha=0.5, ec="black")
     plt.title("Distribution of waiting times")
     plt.legend(loc='upper right')
     plt.xlabel("Waiting times")
     plt.ylabel("Frequency")
     plt.show()
+    
+    # Run the simulations n times to gain statistical insights
+    wait_time_means = []
+    blocked_means = []
+    n = 50
+    for i in range(n):
+        waiting_times, n_blocked = single_queue_multiple_servers_simulation(10000, 10)
+        wait_time_means.append(np.mean(waiting_times))
+        blocked_means.append(np.mean(n_blocked))
+
+    wait_time_lower, wait_time_upper = calculate_confidence_intervals(np.mean(wait_time_means), np.std(blocked_means), n)
+    blocked_lower, blocked_upper = calculate_confidence_intervals(np.mean(blocked_means), np.std(blocked_means), n)
+    
+    print("\n")
+    print("Mean waiting time: {}".format(np.mean(wait_time_means)))
+    print("Lower limit: {}".format(wait_time_lower))
+    print("Upper limit: {}".format(wait_time_upper))
+
+    print("\n")
+    print("Mean blocking rate: {}".format(np.mean(blocked_means)))
+    print("Lower limit: {}".format(blocked_lower))
+    print("Upper limit: {}".format(blocked_upper))
     
 if __name__ == "__main__":
     main()
